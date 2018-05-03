d52dd5df361d60a31cc6f5b0abd387c5e79016e3
madushaninavodya <madushaninavodya6@gmail.com>
2018-05-03 04:43:59 -0700

    #6 Add Gallery Sample Gird

---
+++ gallery.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Gallery Page</title>
+    </head>
+    <body>
+        <h1>Galley Page</h1>
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-4">Image 01</div>
+                    <div class="col-md-4">Image 02</div>
+                    <div class="col-md-4">Image 03</div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
