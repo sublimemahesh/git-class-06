b621bef4f2d4704b926e442c283e9839615a3093
chalanadulaj <chalanadulaj99@gmail.com>
2018-05-14 15:43:09 +0530

    #5 Create about us page

---
+++ About-us.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>About us page</title>
+    </head>
+    <body>
+
+        <h1>Create About us Page</h1>
+        
+        
+    </body>
+</html>
